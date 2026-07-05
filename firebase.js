524f4f81b838cda90f06c1a428025a5b4cd006cf
valentinquisperamos471-code <valentinquisperamos471@gmail.com>
2026-07-05 00:09:21 -0500

    Refactor Firebase setup and ticket storage function
    Updated Firebase configuration and refactored guardarTicket function.

--- firebase.js
+++ firebase.js
@@ -2,32 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, push, set } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAg_v6kN6ZTWXQ-HSMD2aV7bQuh3SNplcs",
-  authDomain: "mira-y-aprende-321c0.firebaseapp.com",
-  databaseURL: "https://mira-y-aprende-321c0-default-rtdb.firebaseio.com",
-  projectId: "mira-y-aprende-321c0",
-  storageBucket: "mira-y-aprende-321c0.appspot.com",
-  messagingSenderId: "1085961832739",
-  appId: "1:1085961832739:web:da32eacb00fbe8c4a5a496"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-export function guardarTicket(ticket){
-
-const nuevoTicket = push(ref(db, "tickets"));
-
-set(nuevoTicket, ticket);
-
-}
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
   apiKey: "AIzaSyAc6J2XPUQhYqkn24ByKelTE6A4pZyWuOg",
   authDomain: "mira-y-aprendev2.firebaseapp.com",
   databaseURL: "https://mira-y-aprendev2-default-rtdb.firebaseio.com",
@@ -37,5 +11,10 @@ const firebaseConfig = {
   appId: "1:339955052391:web:825746f55757bbf02149c2"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export function guardarTicket(ticket) {
+  const nuevoTicket = push(ref(db, "tickets"));
+  return set(nuevoTicket, ticket);
+}
